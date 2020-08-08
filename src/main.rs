acd9276e6fe8913858311cec12340d0121ac2184
Jackson Isenberg <jisenberg1050@gmail.com>
2020-08-08 17:13:23 -0400

    feat: bootload to bare metal

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,30 @@
 #![no_std]      // don't link the Rust standard library
 #![no_main]     // disable all Rust-level entry points
 
-use core::panic::PanicInfo; // panic implementation
+extern crate rlibc;
 
-#[no_mangle]    // don't mangle the name of this function since it is `_start`
-pub extern "C" fn _start() -> ! {
-    // this function is the entry point, since the linker looks for a function
-    // named `_start` by default
-    loop {}
-}
+use core::panic::PanicInfo; // panic implementation
 
 /// This function is called on a panic.
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
+    loop {}
+}
+
+static HELLO: &[u8] = b"Hello, World!";
+
+#[no_mangle]    // don't mangle the name of this function since it is `_start`
+pub extern "C" fn _start() -> ! {
+    let vga_buffer = 0xb8000 as *mut u8;
+
+    for (i, &byte) in HELLO.iter().enumerate() {
+        unsafe {
+            *vga_buffer.offset(i as isize * 2) = byte;
+            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
+        }
+    }
+
+    // this function is the entry point, since the linker looks for a function
+    // named `_start` by default
     loop {}
 }
